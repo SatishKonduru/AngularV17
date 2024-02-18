d09ac42d9fbfc9b90f99eadc980070c366cb6e21
satish <satishkonduru0225@gmail.com>
2024-02-18 17:46:11 +0530

    new

--- Angular_v17/src/app/components/home/home.component.ts
+++ Angular_v17/src/app/components/home/home.component.ts
@@ -9,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  person = {
+    age: 16,
+    place: 'Nandyal',
+    pin: 518502
+  }
+  x = 10;
+  y = 20;
+  sum(){
+    return (this.x+this.y + 100)
+  }
 }
